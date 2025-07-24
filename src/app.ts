cc8865927df284c6ffe37bbe0cf576a7bb29ca3a
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-24 13:31:12 +0700

    fix api real vercel

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app: Application = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", async (req: Request, res: Response) => {
-  res.status(200).json({ message: "API running" }).send("API running");
+  res.send("API running");
 });
 
 app.listen(port, () => {
