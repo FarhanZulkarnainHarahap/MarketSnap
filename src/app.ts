332724ab9d9f973b4e00a776fb44a25d311df685
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-24 13:24:56 +0700

    fix vercel2

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express, { Request, Response, Application } from "express";
 const app: Application = express();
 const port = process.env.PORT || 8080;
 
-app.get("/api/v1/test", async (_req: Request, res: Response) => {
+app.get("/", async (_req: Request, res: Response) => {
   res.status(200).json({ message: "API running" }).send("API running");
 });
 
