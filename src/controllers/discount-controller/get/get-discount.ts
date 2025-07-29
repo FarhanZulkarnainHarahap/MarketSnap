330061138aad261de1acee37b13479e1eeb19aa3
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 12:04:27 +0700

    fix vercel

--- src/controllers/discount-controller/get/get-discount.ts
+++ src/controllers/discount-controller/get/get-discount.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "@/config/prisma-client.js"; // ✅ Perbaikan dari alias error
 import { Prisma } from "@/generated/prisma/index.js";
-// ✅ Perbaikan dari alias error
 import { CustomJwtPayload } from "../../../types/express.js"; // ✅ Pakai relative path, bukan '@/'
 
 const buildDiscountWhereClause = async (
