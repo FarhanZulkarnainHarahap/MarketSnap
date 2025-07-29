431710db60944d0754b0dc61ce656d7edddbd99d
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 12:12:57 +0700

    fix vercel.json2

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
-import app from "../src/app.js";
-
-export default app;
+// api/index.ts
+import { createServer } from "http";
+// pastikan `app` diexport dari src/app.ts
+import app from "../src/app.js"; // pastikan path ini sesuai dengan struktur proyek Anda
+export default async function handler(req: any, res: any) {
+  const server = createServer(app);
+  server.emit("request", req, res);
+}
