3650ae2c6c25f6d0785c9a7d4474c7a09ab8fce7
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 12:28:24 +0700

    fix vercel8

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,3 @@
-// api/index.ts
-import { createServer } from "http";
-// pastikan `app` diexport dari src/app.ts
-import app from "../src/app.js"; // pastikan path ini sesuai dengan struktur proyek Anda
-export default async function handler(req: any, res: any) {
-  const server = createServer(app);
-  server.emit("request", req, res);
-}
+import app from "../src/app.js";
+
+export default app;
