f494c9277ef469d4f6ad24e4f2bff3882930c882
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 13:58:32 +0700

    fix upload cloudinary

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,25 +1,30 @@
+// upload.ts
 import express from "express";
 import multer from "multer";
 import cloudinary from "../config/cloudinary-config.js";
 import { UploadApiResponse } from "cloudinary";
 import streamifier from "streamifier";
+import fs from "fs";
+import path from "path";
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post("/upload", upload.single("paymentProof"), async (req, res) => {
   try {
-    const data = JSON.parse(req.body.data);
-
     if (!req.file || !req.file.buffer) {
       return res.status(400).json({ error: "No file uploaded." });
     }
+
+    const data = JSON.parse(req.body.data);
 
     const result: UploadApiResponse = await new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
         { folder: "upload-payment-proof" },
         (error, result) => {
-          if (error) return reject(error);
+          if (error || !result) return reject(error);
           resolve(result as UploadApiResponse);
         }
       );
