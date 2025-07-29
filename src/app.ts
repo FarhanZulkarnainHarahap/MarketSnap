a6f750a9755ad06efc1a23b80929ccf41d3f2ec1
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 13:32:36 +0700

    fix app

--- src/app.ts
+++ src/app.ts
@@ -85,7 +85,7 @@ app.get("/confirm-email-view", (req: Request, res: Response) => {
   });
 });
 
-app.get("/", async (request: Request, response: Response) => {
+app.get("/api/health", async (request: Request, response: Response) => {
   response.status(200).json({
     message: "API is running",
     uptime: `${process.uptime().toFixed(2)} seconds`,
