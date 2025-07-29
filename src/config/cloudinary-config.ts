d375ef7a920a364f824b06b9e1391a427cff33b1
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-29 15:07:29 +0700

    fix cloudinary

--- src/config/cloudinary-config.ts
+++ src/config/cloudinary-config.ts
@@ -1,9 +1,23 @@
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (_req, file) => {
+    return {
+      folder: "Market-Snap",
+      format: file.mimetype.split("/")[1], // contoh: jpg, png, webp
+      transformation: [{ width: 800, height: 800, crop: "limit" }],
+    };
+  },
+});
+
+export const upload = multer({ storage });
 export default cloudinary;
--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,5 +1,7 @@
 // upload.ts
 import express from "express";
+
+import { Request, Response } from "express";
 import multer from "multer";
 import cloudinary from "../config/cloudinary-config.js";
 import { UploadApiResponse } from "cloudinary";
@@ -12,37 +14,44 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 export const upload = multer({ storage });
 
-router.post("/upload", upload.single("paymentProof"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-
-    const data = JSON.parse(req.body.data);
-    const fileBuffer = req.file.buffer;
-
-    const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: "upload-payment-proof" },
-      (error, result) => {
-        if (error) {
-          return res
-            .status(500)
-            .json({ error: "Upload failed", details: error });
-        }
-
-        res.json({
-          message: "Uploaded successfully",
-          imageUrl: result?.secure_url,
-          data,
-        });
+interface MulterRequest extends Request {
+  file: Express.Multer.File;
+}
+router.post(
+  "/upload",
+  upload.single("paymentProof"),
+  async (req: MulterRequest, res: Response) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
       }
-    );
 
-    streamifier.createReadStream(fileBuffer).pipe(uploadStream);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Upload failed" });
+      const data = JSON.parse(req.body.data);
+      const fileBuffer = req.file.buffer;
+
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "upload-payment-proof" },
+        (error, result) => {
+          if (error) {
+            return res
+              .status(500)
+              .json({ error: "Upload failed", details: error });
+          }
+
+          res.json({
+            message: "Uploaded successfully",
+            imageUrl: result?.secure_url,
+            data,
+          });
+        }
+      );
+
+      streamifier.createReadStream(fileBuffer).pipe(uploadStream);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Upload failed" });
+    }
   }
-});
+);
 
 export default router;
